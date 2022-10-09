33a6f43a069b9c80ce5a307aeadb18a2d7986bd9
jantunenv <ville.e.jantunen@gmail.com>
2022-10-10 01:54:02 +0300

    removed commented lines from image_downloader

--- image_downloader.py
+++ image_downloader.py
@@ -5,8 +5,6 @@ def main():
 	with open("kansallisgalleria.json", 'r') as f:
 		data = json.load(f)
 
-#	print(data[0].keys())
-
 	for object in data:
 		id = object["objectId"]
 		if(object["multimedia"]):
@@ -14,7 +12,6 @@ def main():
 			filedata = urllib.request.urlopen(link)
 			image_data = filedata.read()
 			filename = object["multimedia"][0]["filename"]
-#			print(filename)
 
 			with open('figures/'+filename, 'wb') as f:
 				f.write(image_data)
